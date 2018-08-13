0b1f9e126f73588a8a023865e35992e8cad4676c
q-hwang <qh53@cornell.edu>
2018-08-13 10:02:11 +0000

    avocado dataset

--- preprocess.py
+++ preprocess.py
@@ -55,6 +55,6 @@ if __name__ == "__main__":
 
     print("Sampling frequency: {}".format(hparams.sample_rate))
 
-    assert name in ["cmu_arctic", "ljspeech", "librivox", "thchs"]
+    assert name in ["cmu_arctic", "ljspeech", "librivox", "thchs", "avocado"]
     mod = importlib.import_module(name)
     preprocess(mod, in_dir, out_dir, num_workers)
